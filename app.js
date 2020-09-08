b8051411f3e9c60c1d8a20e8e8ae3bf0d7bb51dd
Glennch <glenn.chon@gmail.com>
2020-09-08 13:45:40 -0400

    removed routes

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
 "use strict";
-
-require("dotenv").config();
+const bodyParser = require("body-parser");
+const express = require("express");
+const http = require("http");
 const OS = require("os");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const app = express();
+// Threadpool config
+require("dotenv").config();
 process.env.UV_THREADPOOL_SIZE = OS.cpus().length;
 
-const http = require("http");
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-
 // Middleware
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>'
-  );
-});
-
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
-});
-
+app.use("admin/", adminRoutes);
+app.use(shopRoutes);
 // Router
 
 // Server
